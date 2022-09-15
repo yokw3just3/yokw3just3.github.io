719b7c25593cf2f36e4cc5c3beb307fd73265565
Steve Yonkeu <yokwejuste@yahoo.com>
2022-09-15 07:21:17 +0100

    feat: made updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,35 @@
 import React from "react";
-import '../styles/Header.css';
+import "../styles/Header.css";
 import MenuBar from "../components/MenuBar";
-import react_icon from '../images/react_icon.svg'
-import js_icon from '../images/js_icon.svg'
-import html_icon from '../images/html_icon.svg'
-import css_icon from '../images/css_icon.svg'
-import json_icon from '../images/json_icon.svg'
-
+import react_icon from "../images/react_icon.svg";
+import js_icon from "../images/js_icon.svg";
+import html_icon from "../images/html_icon.svg";
+import css_icon from "../images/css_icon.svg";
+import json_icon from "../images/json_icon.svg";
+// import python_icon from "../images/python.png";
+import markdown_icon from "../images/markdown.png";
 
 function Header() {
-  return ( 
+  return (
     <header className="header-container">
       <nav className="nav-container">
-        <MenuBar 
-          icon={ react_icon }
-          name='home.jsx'
-          path="/"
-        /> 
-        <MenuBar 
-          icon={ html_icon }
-          name='about.html'
-          path="/about"
-        />
-        <MenuBar 
-          icon={ js_icon }
-          name='projects.js'
-          path="/projects"
-        />
-        <MenuBar 
-          icon={ json_icon }
-          name='recommendations.json'
+        <MenuBar icon={react_icon} name="home.jsx" path="/" />
+        <MenuBar icon={html_icon} name="about.html" path="/about" />
+        <MenuBar icon={js_icon} name="projects.js" path="/projects" />
+        <MenuBar
+          icon={json_icon}
+          name="recommendations.json"
           path="/recommendation"
-        /> 
+        />
+        <MenuBar icon={css_icon} name="contact.css" path="/contact" />
+        {/* 
         <MenuBar 
-          icon={ css_icon }
-          name='contact.css'
-          path="/contact"
-        /> 
+          icon={ python_icon }
+          name='more.py'
+          path="/more"
+        />  
+        */}
+        <MenuBar icon={markdown_icon} name="blog.md" path="/blog" />
       </nav>
     </header>
   );
