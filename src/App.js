6cae1ab4b0b309b46cb4bad346983fb9e9ba4db4
Steve Yonkeu <yokwejuste@yahoo.com>
2022-09-16 02:37:23 +0100

    feat: added route for followers

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Projects from "./pages/Projects";
 import Recommendation from "./pages/Recommendation";
+import Followers from "./pages/Followers";
 import AppProvider from "./context/AppProvider";
-
 
 function App() {
   return (
-      <AppProvider>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/recommendation" element={<Recommendation />} />
-          {/* <Route path="/more" element={<Recommendation />} /> */}
-          <Route path="/blog" element={<Blog/>} />
-        </Routes>
-      </AppProvider>
+    <AppProvider>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/recommendation" element={<Recommendation />} />
+        <Route path="/followers" element={<Followers />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </AppProvider>
   );
 }
 
